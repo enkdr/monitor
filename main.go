7aee1e9bfaacfef3f2a53e6e8f43567132f4258d
Raphael Chambers <raphaelchambers@gmail.com>
2024-06-17 13:16:04 +1000

    init

---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"syscall"
+	"time"
+)
+
+type Fsid struct {
+	X__val [2]int32 `json:"fsid"`
+}
+
+type FileSystemStats struct {
+	Type    int64  `json:"type"`
+	Bsize   int64  `json:"block_size"`
+	Blocks  uint64 `json:"total_blocks"`
+	Bfree   uint64 `json:"free_blocks"`
+	Bavail  uint64 `json:"available_blocks"`
+	Files   uint64 `json:"total_files"`
+	Ffree   uint64 `json:"free_files"`
+	Fsid    Fsid
+	Namelen int64    `json:"max_name_length"`
+	Frsize  int64    `json:"fragment_size"`
+	Flags   int64    `json:"flags"`
+	Spare   [4]int64 `json:"spare"`
+}
+
+func monitorDiskUsage(path string, interval time.Duration) {
+
+	fs := syscall.Statfs_t{}
+
+	err := syscall.Statfs(path, &fs)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("%+v\n", fs)
+
+	stats := FileSystemStats{
+		Type:    fs.Type,
+		Bsize:   fs.Bsize,
+		Blocks:  fs.Blocks,
+		Bfree:   fs.Bfree,
+		Bavail:  fs.Bavail,
+		Files:   fs.Files,
+		Ffree:   fs.Ffree,
+		Fsid:    Fsid{X__val: fs.Fsid.X__val},
+		Namelen: fs.Namelen,
+		Frsize:  fs.Frsize,
+		Flags:   fs.Flags,
+		Spare:   fs.Spare,
+	}
+
+	jsonData, err := json.MarshalIndent(stats, "", " ")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(jsonData))
+
+}
+
+func main() {
+	var interval int
+
+	if len(os.Args) < 3 {
+		fmt.Println("need to specify directory and interval")
+		os.Exit(0)
+	}
+
+	path := os.Args[1]
+
+	if _, err := fmt.Sscanf(os.Args[2], "%d", &interval); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	tick := time.Duration(interval) * time.Second
+
+	// more precise than sleep
+	ticker := time.NewTicker(tick)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println("tick at: ", time.Now())
+			monitorDiskUsage(path, time.Duration(interval))
+		}
+	}
+
+	// for {
+	// 	time.Sleep(tick)
+	// 	fmt.Println("tick at: ", time.Now())
+	// }
+
+}
