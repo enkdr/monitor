306a918a17ebadc2c731f140e5fcb29c32193a92
Raphael Chambers <raphaelchambers@gmail.com>
2024-06-21 09:35:36 +1000

    indentation ..

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,28 +1,28 @@
 console.log("M O N I T O R");
 
- document.addEventListener("DOMContentLoaded", function () {
-            const dataElement = document.querySelector('.stats');
-            let eventSource;
+document.addEventListener("DOMContentLoaded", function () {
+    const dataElement = document.querySelector('.stats');
+    let eventSource;
 
-            function connect() {
-                eventSource = new EventSource('http://localhost:8080/stats');
+    function connect() {
+        eventSource = new EventSource('http://localhost:8080/stats');
 
-                eventSource.onmessage = function (event) {
-                    dataElement.innerHTML += JSON.stringify(event.data, null, 2) + '<br/>';
-                };
+        eventSource.onmessage = function (event) {
+            dataElement.innerHTML += JSON.stringify(event.data, null, 2) + '<br/>';
+        };
 
-                eventSource.onerror = function () {
-                    console.log('Error occurred, reconnecting...');
-                    eventSource.close();
-                    setTimeout(connect, 2000); // Attempt to reconnect after 2 seconds
-                };
+        eventSource.onerror = function () {
+            console.log('Error occurred, reconnecting...');
+            eventSource.close();
+            setTimeout(connect, 2000); // Attempt to reconnect after 2 seconds
+        };
 
-                eventSource.onopen = function () {
-                    console.log('Connected');
-                    
-                };
-            }
+        eventSource.onopen = function () {
+            console.log('Connected');
+            
+        };
+    }
 
-            connect(); // Initial connection
+    connect(); // Initial connection
 
-        });
+});
